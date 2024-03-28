a7ceded79d6882900b0b249c4bcff039b3111c04
lizzzshan <odysuessrox@gmail.com>
2024-03-28 15:40:53 -0700

    Part 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
 
+//TODO: Put this in components document
+const Person = (props) => {
+  //console.log(props.person[0].name)
+  // Map to the name and number to display persons obj
+  const name = props.person.map(prop => prop.name)
+  const number = props.person.map(prop => prop.number)
+  console.log(number)
+  //console.log(name)
+  return(
+    <div>
+      {name} {number}
+    </div>
+  )
+}
+
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) =>{
     event.preventDefault()
     //console.log('button clicked', event.target)
-    
     const newPerson = {
       name: newName,
       number: newNumber
@@ -18,21 +36,23 @@ const App = () => {
 
     //console.log(persons.name)
     const checkName = persons.find(props => props.name.toLowerCase() === newPerson.name.toLowerCase())
-    console.log(checkName)
+    const checkNum = persons.find(props=> props.number == newPerson.number)
 
     // If checkName is not undefined, duplicate already exists. Give an alert and do not add
     if (checkName){
       alert(`${newName} is already added to phonebook`)
     }
+    // Check if number already exists within persons obj
+    else if (checkNum){
+      alert(`${newNumber} already exists in the phonebook`)
+    }
     else{
       //Add new person to persons
       setPersons(persons.concat(newPerson))
+      // This resets the input values (I think)
       setNewName('')
+      setNewNumber('')
     }
-
-    //console.log(persons)
-
-    //console.log(persons)
   }
 
   const handleNameChange = (event) =>{
@@ -52,14 +72,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNewNumber } />
+          number: <input value={newNumber} onChange={handleNewNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{newName} {persons.number}</div>
+      <div>
+        <Person person={persons}/>
+      </div>
     </div>
   )
 }
