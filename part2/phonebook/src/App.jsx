d5a7c52ef3f74adc394d51a61d4c3e898e4ee488
lizzzshan <odysuessrox@gmail.com>
2024-03-28 11:59:46 -0700

    Insert numbers input feature

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,18 +5,20 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) =>{
     event.preventDefault()
     //console.log('button clicked', event.target)
     
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     //console.log(persons.name)
     const checkName = persons.find(props => props.name.toLowerCase() === newPerson.name.toLowerCase())
-    //console.log(checkName)
+    console.log(checkName)
 
     // If checkName is not undefined, duplicate already exists. Give an alert and do not add
     if (checkName){
@@ -28,7 +30,7 @@ const App = () => {
       setNewName('')
     }
 
-    console.log(persons)
+    //console.log(persons)
 
     //console.log(persons)
   }
@@ -36,6 +38,10 @@ const App = () => {
   const handleNameChange = (event) =>{
     //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -46,12 +52,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber } />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
-      ...
+      <div>{newName} {persons.number}</div>
     </div>
   )
 }
