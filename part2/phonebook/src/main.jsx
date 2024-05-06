eb42060e21191b4bd29c83cc9c510a0760192e6e
lizzzshan <odysuessrox@gmail.com>
2024-05-06 09:34:22 -0700

    Add Axios

--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/persons')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
